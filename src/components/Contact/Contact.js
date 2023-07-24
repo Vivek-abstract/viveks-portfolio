b64f5987fc136fe49d540f7e192ae2060c96ecd2
Vivek Gawande <vivek040997@gmail.com>
2023-07-24 13:03:15 +0530

    Update github link and resume link

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,16 +3,13 @@ import resume from '../../assets/Vivek_Gawande_Resume.pdf';
 
 export default function Contact() {
 
-    const openPdf = () => {
-        window.open(resume, '_blank', 'fullscreen=yes');
-    }
     return (
         <div className="container mt-5 d-flex justify-content-center contact-container">
             <div className="contact text-center mb-3"><h2 className='contact-text'>Contact</h2></div>
             <div className='d-flex mb-5'>
-                <h5 className='me-5'><a className="link" target="_blank" href="https://www.instagram.com/vivekgawande/">Instagram</a></h5>
+                <h5 className='me-5'><a className="link" target="_blank" href="https://github.com/Vivek-abstract">GitHub</a></h5>
                 <h5 className='me-5'><a className="link"target="_blank" href="https://www.linkedin.com/in/vivekbgawande/">LinkedIn</a></h5>
-                <h5><a className="link"  href="#"onClick={openPdf}>Resume</a></h5>
+                <h5 className='me-5'><a className="link"target="_blank" href="https://bit.ly/ViveksResume">Resume</a></h5>
             </div>
         </div>
     );
